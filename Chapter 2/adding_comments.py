9a2e82bf1afb6c61069855485838d6848ee22d16
GugulethuMthethwa <gugulethumthethwa2508@gmail.com>
2025-01-19 23:01:50 +0200

    Add files via upload

---
+++ Chapter 2/adding_comments.py
@@ -0,0 +1,27 @@
+# Author: [Gugulethu]
+# Date: 2025-01-19
+# This program performs addition, subtraction, multiplication, and division operations 
+# that each result in the number 8.
+
+# Addition
+print(5 + 3)
+
+# Subtraction
+print(16 - 8)
+
+# Multiplication
+print(4 * 2)
+
+# Division
+print(16 / 2)
+
+# Author: [Gugulethu]
+# Date: 2025-01-19
+# This program uses a variable to represent my favorite number and prints a message with that number.
+
+# Store favorite number in a variable
+favorite_number = 7
+
+# Create message and print it
+message = f"My favorite number is {favorite_number}."
+print(message)
