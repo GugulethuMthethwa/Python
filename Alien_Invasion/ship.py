90abf9f23a4292a420dd0b49595bf1dc364e00b3
GugulethuMthethwa <gugulethumthethwa2508@gmail.com>
2025-05-13 19:57:21 +0200

    Chapter_13

---
+++ Alien_Invasion/ship.py
@@ -0,0 +1,44 @@
+import pygame
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = self.screen.get_rect()
+
+        # Load the ship image (ship.bmp)
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on movement flags."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.settings.ship_speed
+
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.center = float(self.screen_rect.centerx)
+        self.rect.centerx = self.center
+
+
